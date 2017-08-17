c86f47d6b81e53746cdf8e55e599e745d12d0f63
Will Saunders <will@cia-adv.com>
2017-08-17 13:16:16 -0700

    v2 layout

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
+    //LOAD FB SDK
+    $.ajaxSetup({ cache: true });
+    $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+        FB.init({
+          appId: '1956267324660722',
+          version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+        });     
+       // $('#loginbutton,#feedbutton').removeAttr('disabled');
+        //FB.getLoginStatus(updateStatusCallback);
+    });
+
+
 	$('#fileupload').one('change', uploadVideo);
     function uploadVideo(e){
         e.preventDefault();
         $('progress').show();
-        $('#create-btn').addClass('disabled').find('input');
+        $('#create-btn').find('img').addClass('disabled');
+        $('#spinner').show();
             $.ajax({
             url:'/upload',
             type: 'POST',
@@ -27,12 +40,34 @@ $(document).ready(function(){
             }
         }).done(function(data){
             console.log(data);
+            $('#spinner').hide();
             $('#preview').find('source').attr('src', data.video_url);
             $('#preview')[0].load();
-            // $('#preview')[0].play();
-            $('#instruct').hide();
+            $('#create').hide();
             $('#share').show();
+
+            //Add S3 url to download button
+            $('#download-btn').find('a').attr('href', data.video_url);
+            $('#facebook-btn').attr('data-url',data.video_url);
             console.log('Time Elapsed: '+data.timeElapsed+' seconds');
         });
     }
+    $('#restart-btn').click(function(){
+        $('#share').hide();
+        $('#create-btn').find('img').removeClass('disabled');
+        $('progress').hide();
+        $('#spinner').hide();
+        $('#create').show();
+    });
+    $('#facebook-btn').click(function(){
+        if(FB){
+            FB.ui({
+              method: 'share',
+              href: $('#facebook-btn').attr('data-url')
+            }, function(response){
+
+                console.log('shared');
+            });
+        }
+    })
 });
